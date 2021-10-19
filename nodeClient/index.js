b20fc1cfe26d5c614f446d81201abec0322b7da7
Joseph Mutua <mutuaj793@gmail.com>
2021-10-19 16:17:34 +0300

    Getting os performance data

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -1,1 +1,34 @@
 //The node program that captures local performancee data
+//and sends it to a socket.io server
+//Requirements:
+//-farmhash
+//-socket.io-client
+
+//What we need to know from node about performance
+//--Current  CPU Load
+//--Memory Usage:: Free, Total, OS Type, Uptime,
+//--CPU Info: Type, No. of Cores, Clock Speed
+const os = require("os");
+
+const osType = os.type();
+console.log("OS Type", osType);
+
+const upTime = os.uptime();
+console.log("UPTIME", upTime);
+
+const freeMem = os.freemem();
+console.log("FREEMEM", freeMem);
+
+const totalMem = os.totalmem();
+console.log("TOTALMEM", totalMem);
+
+const cpus = os.cpus();
+const cpuModel = cpus[0].model
+const cpuSpeed = cpus[0].speed;
+const numCores = cpus.length;
+
+
+
+const usedMem = totalMem - freeMem;
+const memUsage = Math.floor((usedMem / totalMem) * 100) / 100;
+console.log(memUsage);
