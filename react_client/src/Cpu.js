360df0f121693e70b2aab1f7b56808218a7f254b
Joseph Mutua <mutuaj793@gmail.com>
2021-10-23 14:35:40 +0300

    CPU widget canvas

--- react_client/src/Cpu.js
+++ react_client/src/Cpu.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React from "react";
+import drawCircle from "./utilities/canvasLoadAnimation";
 
-function Cpu(props){
-    return (
-        <h3>Cpu!</h3>
-    )
+function Cpu(props) {
+  const canvas = document.querySelector("canvas");
+  drawCircle(canvas, props.cpuData.cpuLoad)
+  console.log(props);
+  return (
+    <div className="col-sm-3 cpu">
+      <h3>CPU LOAD</h3>
+      <div className="canvas-wrapper">
+        <canvas className="canvas"></canvas>
+        <div className="cpu-text">{props.cpuData.cpuLoad}</div>
+      </div>
+    </div>
+  );
 }
+
 export default Cpu;
